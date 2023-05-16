722525efb78a0dc85e962bbf31ff127588d694da
Jose Fernandez <jmfernandezg.awe@gmail.com>
2023-05-15 18:49:05 -0600

    Made into a gradle application :tada:

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "com.jmfg.training.kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:31.1-jre")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    mainClass.set("com.jmfg.training.kotlin.designpatterns.AppKt")
+}
